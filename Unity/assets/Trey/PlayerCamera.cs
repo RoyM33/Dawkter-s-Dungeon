0d5f0fb34c6855e455aac32ec14db2d12f52b3a1
TreyWheeler <Warenth@gmail.com>
2014-01-25 09:08:48 -0600

    Added Test Scene

---
+++ Unity/assets/Trey/PlayerCamera.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour
+{
+
+    public GameObject FirstPersonCameraDock;
+    public GameObject TopDownCameraDock;
+    public PlayerCharacter Player;
+    public float CameraTransitionSpeed = 5f;
+
+    private GameObject FromCameraObject;
+    private GameObject TargetCameraObject;
+    private MouseLook CameraMouseLook;
+
+    // Use this for initialization
+    void Start()
+    {
+        TargetCameraObject = FirstPersonCameraDock;
+        FromCameraObject = TopDownCameraDock;
+
+        CameraMouseLook = GetComponent<MouseLook>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player.ViewOption == PlayerViewOptions.TopDown)
+        {
+            TargetCameraObject = FirstPersonCameraDock;
+            FromCameraObject = TopDownCameraDock;
+        }
+        else
+        {
+            TargetCameraObject = TopDownCameraDock;
+            CameraMouseLook.enabled = false;
+            FromCameraObject = FirstPersonCameraDock;
+
+        }
+
+        if (this.transform.position != TargetCameraObject.transform.position)
+        {
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, TargetCameraObject.transform.rotation, Time.deltaTime * CameraTransitionSpeed);
+            this.transform.position = Vector3.Slerp(this.transform.position, TargetCameraObject.transform.position, Time.deltaTime * CameraTransitionSpeed);
+
+            if (this.transform.position == TargetCameraObject.transform.position && TargetCameraObject == FirstPersonCameraDock)
+            { // Destination Reached
+                CameraMouseLook.enabled = true;
+            }
+        }
+    }
+}
--- Unity/assets/Trey/PlayerCharacter.cs
+++ Unity/assets/Trey/PlayerCharacter.cs
@@ -1,20 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerCharacter : Character {
+public class PlayerCharacter : Character
+{
+    public PlayerViewOptions ViewOption = PlayerViewOptions.OverTheShoulder;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (ViewOption == PlayerViewOptions.TopDown)            
+            {
+                ViewOption = PlayerViewOptions.OverTheShoulder;
+
+            }
+            else
+            {
+                ViewOption = PlayerViewOptions.TopDown;
+
+            }
+
+         }
+
+
+    }
 
     void OnGUI()
     {
         GUI.Box(new Rect(0, 0, Screen.width * Health / 100, 30), Health.ToString());
     }
+}
+
+
+public enum PlayerViewOptions
+{
+    TopDown, OverTheShoulder
 }
