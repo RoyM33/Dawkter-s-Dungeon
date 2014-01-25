a01a0dd12ff5c023236ed6f0b455837c7a48e95a
RoyM33 <deltasniper864@hotmail.com>
2014-01-25 16:09:10 -0600

    Switches

---
+++ Unity/assets/Roy/ColorObject.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorObject : MonoBehaviour {
+
+    private GlobalObjectUpdating _controller;
+    public Colors myColor;
+	void Start () {
+        _controller = GameObject.FindObjectOfType<GlobalObjectUpdating>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider objectColliding)
+    {
+        Debug.Log("entered");
+        if (objectColliding.tag == "Player")
+        {
+            _controller.currentColor = myColor;
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+}
--- Unity/assets/Roy/GlobalObjectUpdating.cs
+++ Unity/assets/Roy/GlobalObjectUpdating.cs
@@ -5,47 +5,135 @@ using System.Linq;
 
 public class GlobalObjectUpdating : MonoBehaviour {
 
-    public Colors currentColor = Colors.blue;
-    private List<GameObject> _blueObjects;
-    private List<GameObject> _redObjects;
-    private List<GameObject> _yellowObjects;
-    public float flashIntervals = .5f;
-    private float currentInterval = 0;
-	// Use this for initialization
-	void Start () {
-        _blueObjects = GameObject.FindGameObjectsWithTag("Blue").ToList();
-        Debug.Log(_blueObjects.Count);
-        _redObjects = GameObject.FindGameObjectsWithTag("Red").ToList();
-        _yellowObjects = GameObject.FindGameObjectsWithTag("Yellow").ToList();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        currentInterval += Time.deltaTime;
-        if (currentInterval >= flashIntervals)
+    private Colors _currentColor;
+    public Colors currentColor
+    { 
+        get { return _currentColor; }
+        set
         {
-            switch (currentColor)
+            startBlinking = true;
+            _currentColor = value; 
+            switch (value)
             {
                 case Colors.red:
-                    break;
-                case Colors.blue:
                     foreach (var item in _blueObjects)
                     {
-                        var mesh = item.GetComponent<MeshRenderer>();
-                        if (mesh)
-                        {
-                            mesh.enabled = !mesh.enabled;
-                        }
+                        AdjustMeshToTrue(item);
+                        item.GetComponent<BoxCollider>().isTrigger = false;
+                    }
+                    foreach (var item in _yellowObjects)
+                    {
+                        AdjustMeshToTrue(item);
+                        item.GetComponent<BoxCollider>().isTrigger = false;
+                    }
+                    break;
+                case Colors.blue:
+                    foreach (var item in _redObjects)
+                    {
+                        AdjustMeshToTrue(item);
+                        item.GetComponent<BoxCollider>().isTrigger = false;
+                    }
+                    foreach (var item in _yellowObjects)
+                    {
+                        AdjustMeshToTrue(item);
+                        item.GetComponent<BoxCollider>().isTrigger = false;
                     }
                     break;
                 case Colors.yellow:
-                    break;
-                default:
+                    foreach (var item in _blueObjects)
+                    {
+                        AdjustMeshToTrue(item);
+                        item.GetComponent<BoxCollider>().isTrigger = false;
+                    }
+                    foreach (var item in _redObjects)
+                    {
+                        AdjustMeshToTrue(item);
+                        item.GetComponent<BoxCollider>().isTrigger = false;
+                    }
                     break;
             }
-            currentInterval = 0;
+        }
+    }
+    private bool startBlinking = false;
+    private List<GameObject> _blueObjects = new List<GameObject>();
+    private List<GameObject> _redObjects=new List<GameObject>();
+    private List<GameObject> _yellowObjects=new List<GameObject>();
+    public float flashIntervals = .5f;
+    private float currentInterval = 0;
+
+	void Start () {
+       _blueObjects = GameObject.FindGameObjectsWithTag("Blue").ToList();
+        _redObjects = GameObject.FindGameObjectsWithTag("Red").ToList();
+        _yellowObjects = GameObject.FindGameObjectsWithTag("Yellow").ToList();
+       }
+	
+	void Update () {
+        if(startBlinking)
+        {
+            currentInterval += Time.deltaTime;
+            if (currentInterval >= flashIntervals)
+            {
+                switch (currentColor)
+                {
+                    case Colors.red:
+                        foreach (var item in _redObjects)
+                        {
+                            AdjustMeshToOpposite(item);
+                            item.GetComponent<BoxCollider>().isTrigger = true;
+                        }
+                        break;
+                    case Colors.blue:
+                        foreach (var item in _blueObjects)
+                        {
+                            AdjustMeshToOpposite(item);
+                            item.GetComponent<BoxCollider>().isTrigger = true;
+                        }
+                        break;
+                    case Colors.yellow:
+                        foreach (var item in _yellowObjects)
+                        {
+                            AdjustMeshToOpposite(item);
+                            item.GetComponent<BoxCollider>().isTrigger = true;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                currentInterval = 0;
+            }
         }
 	}
+
+    private static void AdjustMeshToTrue(GameObject item)
+    {
+        var mesh = item.GetComponent<MeshRenderer>();
+        if (mesh)
+        {
+            mesh.enabled = true;
+        }
+    }
+
+    private static void AdjustMeshToOpposite(GameObject item)
+    {
+        var mesh = item.GetComponent<MeshRenderer>();
+        if (mesh)
+        {
+            mesh.enabled = !mesh.enabled;
+        }
+    }
+
+    public void AddRed(GameObject itemToAdd)
+    {
+        _redObjects.Add(itemToAdd);
+    }
+    public void AddYellow(GameObject itemToAdd)
+    {
+        _blueObjects.Add(itemToAdd);
+    }
+    public void AddBlue(GameObject itemToAdd)
+    {
+        _yellowObjects.Add(itemToAdd);
+    }
 }
 
 public enum Colors {
