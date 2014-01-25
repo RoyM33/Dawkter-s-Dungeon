dc6aa99d5540cd360ba6859a280b5387248db4ba
stick100@hotmail.com <stick100@hotmail.com>
2014-01-25 16:33:45 -0600

    Sounds

--- Unity/assets/Mark/Shoot.cs
+++ Unity/assets/Mark/Shoot.cs
@@ -3,38 +3,40 @@ using System.Collections;
 
 public class Shoot : MonoBehaviour
 {
-
-    public double timebetweenshots = 1;
-    private double lastfiretime = 0;
-    public GameObject Bullet;
-    private PlayerCharacter _player;
-
-    // Use this for initialization
-    void Start()
-    {
-        _player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCharacter>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        lastfiretime += Time.deltaTime;
-
-        if (_player.ViewOption == PlayerViewOptions.OverTheShoulder)
-        {
-            if (lastfiretime > timebetweenshots && Input.GetButton("Fire1") == true)
-            {
-                var clone = Instantiate(Bullet, Camera.main.transform.position, Camera.main.transform.rotation);
-                Destroy(clone, 1);
-            }
-        }
-    }
-
-    void OnCollisionEnter(Collision col)
-    {
-        if (col.gameObject.tag == "Enemy")
-        {
-            col.gameObject.transform.localPosition.Set(10, 10, 0);// = 10;
-        }
-    }
+	
+	public double timebetweenshots = .1;
+	public double timebetweenSounds = 1;
+	private double lastfiretime = 0;
+	private double lastSoundtime = 0;
+	public GameObject Bullet;
+	public AudioClip Audio;
+	public AudioSource audioS;
+	private PlayerCharacter _player;
+	
+	// Use this for initialization
+	void Start()
+	{
+		_player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCharacter>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		lastfiretime += Time.deltaTime;
+		lastSoundtime += Time.deltaTime;
+		if(lastfiretime > timebetweenshots && Input.GetButton("Fire1") == true&& _player.ViewOption == PlayerViewOptions.OverTheShoulder)
+		{
+			lastfiretime = 0;
+			var clone  = Instantiate(Bullet,Camera.main.transform.position,Camera.main.transform.rotation);
+			
+			if(lastSoundtime > timebetweenshots)
+			{
+				lastSoundtime = 0;
+				this.audioS.PlayOneShot(Audio);
+			}
+			Destroy(clone, 1);
+		}
+		
+	}
+	
 }
