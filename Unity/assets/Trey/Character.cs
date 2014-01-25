26fefd697e46477d65f012ab200e46c719e4beb0
TreyWheeler <Warenth@gmail.com>
2014-01-25 17:44:42 -0600

    tweaked doors and switches prefabs

--- Unity/assets/Trey/Character.cs
+++ Unity/assets/Trey/Character.cs
@@ -1,26 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Character : MonoBehaviour {
+public class Character : MonoBehaviour
+{
 
     public float Health = 100;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
 
-	void OnTriggerEnter (Collider other)
-	{
-		Health -= 10.01f;//10 shots to kill
-		if (Health < 0 && other.name.Equals("Bullet(Clone)"))
-		{
-			Destroy(this.gameObject);
-		}
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name.Equals("Bullet(Clone)"))
+        {
+            Health -= 10.01f;//10 shots to kill
+            if (Health < 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
 }
