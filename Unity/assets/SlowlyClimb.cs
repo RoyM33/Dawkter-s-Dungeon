b2ca6d1efc7cfd1a7589a19d21af264637b6dddc
stick100@hotmail.com <stick100@hotmail.com>
2014-01-26 14:28:01 -0600

    Flying enemy

---
+++ Unity/assets/SlowlyClimb.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlowlyClimb : MonoBehaviour {
+
+	public float speedup = .001f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate() {
+		if(this.gameObject.transform.position.y < 25)
+		{
+			this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x, (this.gameObject.transform.position.y+(speedup/Time.deltaTime)), this.gameObject.transform.position.z);
+		}
+	}
+}
