3895521f0bb122a590d6ba36fcbc1af977e57d6f
TreyWheeler <Warenth@gmail.com>
2014-01-25 19:57:34 -0600

    Added Lose Condition

---
+++ Unity/assets/Trey/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Extensions
+{
+
+    public static T EnsureComponent<T>(this GameObject gameObj) where T : Component
+    {
+        T component = gameObj.GetComponent<T>();
+
+        if (component == null)
+            component = gameObj.AddComponent<T>();
+
+        return component;
+    }
+
+    public static T EnsureComponent<T>(this MonoBehaviour behavior) where T : Component
+    {
+        return behavior.gameObject.EnsureComponent<T>();
+    }
+}
