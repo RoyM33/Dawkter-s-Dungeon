e9cf1f8f856010043b9252c1468578ba19bdb5dc
TreyWheeler <Warenth@gmail.com>
2014-01-26 13:34:39 -0600

    Added Start

--- Unity/assets/Trey/Game.cs
+++ Unity/assets/Trey/Game.cs
@@ -9,7 +9,22 @@ public class Game : MonoBehaviour
     private Texture YouWinTexture;
     private Texture YouLoseTexture;
 
+    private Texture Ready0;
+    private Texture Ready1;
+    private Texture Ready2;
+    private Texture Ready3;
+
+
     private AudioClip _onLoseSound;
+
+    private float IntroStart;
+    public float TimeSinceStart
+    {
+        get
+        {
+            return Time.realtimeSinceStartup - IntroStart;
+        }
+    }
 
     // Use this for initialization
     void Start()
@@ -21,10 +36,18 @@ public class Game : MonoBehaviour
 
         YouWinTexture = Resources.Load<Texture>("Images/YouWin");
         YouLoseTexture = Resources.Load<Texture>("Images/YouLose");
+        Ready0 = Resources.Load<Texture>("Images/0");
+        Ready1 = Resources.Load<Texture>("Images/1");
+        Ready2 = Resources.Load<Texture>("Images/2");
+        Ready3 = Resources.Load<Texture>("Images/3");
 
         this.EnsureComponent<AudioSource>();
 
         _onLoseSound = Resources.Load<AudioClip>("Sounds/onLose");
+
+        Time.timeScale = 0;
+        IntroStart = Time.realtimeSinceStartup;
+
     }
 
     // Update is called once per frame
@@ -33,7 +56,12 @@ public class Game : MonoBehaviour
         switch (_gameState)
         {
             case GameState.Intro:
-                _gameState = GameState.InProgress;
+
+                if (TimeSinceStart > 4)
+                {
+                    Time.timeScale = 1;
+                    _gameState = GameState.InProgress;
+                }
                 break;
 
 
@@ -60,12 +88,19 @@ public class Game : MonoBehaviour
         switch (_gameState)
         {
             case GameState.Intro:
-                _gameState = GameState.InProgress;
+                if (TimeSinceStart > 3)
+                    Ready1.DrawCentered();
+                else if (TimeSinceStart > 2)
+                    Ready2.DrawCentered();
+                else if (TimeSinceStart > 1)
+                    Ready3.DrawCentered();
+                else
+                    Ready0.DrawCentered();
                 break;
 
 
             case GameState.InProgress:
-                
+
                 break;
 
 
